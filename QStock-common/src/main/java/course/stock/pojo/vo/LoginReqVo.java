26a64a66ce5a363d7ecb312a6ff7c40ab27994e3
1115cr <3214774153@qq.com>
2026-02-07 18:11:46 +0800

    init project

---
+++ QStock-common/src/main/java/course/stock/pojo/vo/LoginReqVo.java
@@ -0,0 +1,24 @@
+package course.stock.pojo.vo;
+
+import lombok.Data;
+
+@Data
+public class LoginReqVo {
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 验证码
+     */
+    private String code;
+
+    /**
+     * 存入redis的随机码的key
+     */
+    //private String sessionId;
+}
